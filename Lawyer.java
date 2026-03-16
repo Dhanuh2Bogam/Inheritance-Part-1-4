fe4bba41a6f6385f81e052931233953edf04bb07
Dhanuh Bogam <154802050+Dhanuh2Bogam@users.noreply.github.com>
2026-03-16 05:48:55 -0700

    Create Lawyer.java

---
+++ Lawyer.java
@@ -0,0 +1,18 @@
+// The lawyer class extends the Employee class.
+public class Lawyer extends Employee {
+    
+    
+    // It overrides the getVacationDays method 
+    // that is found in the Employee class.
+    // A Lawyer gets 1 extra week of vacation beyond what
+    // the standard employee gets
+    // TODO: override the method getVacationDays
+
+
+    // TODO: add a Lawyer specific method called, sue.
+    // It will simply print out, "I'll see you in court!"
+
+
+    // TODO: a Lawyer will work 20 hours per week more than
+    // a regular employee.
+}
