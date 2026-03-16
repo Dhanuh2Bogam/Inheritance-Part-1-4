f7d13de36f9668fafef0d93d64549269e367f3f0
Dhanuh Bogam <154802050+Dhanuh2Bogam@users.noreply.github.com>
2026-03-16 05:50:01 -0700

    Create LegalSecretary.java

---
+++ LegalSecretary.java
@@ -0,0 +1,7 @@
+// Implement the LegalSecretary class.
+// A LegalSecretary gets all the behavior that a Secretary does.
+// But, a LegalSecretary gets 25% more salary.
+public class LegalSecretary {
+    
+}
+
